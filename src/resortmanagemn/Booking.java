7fb344be6d3bd964f4e6a465246740d7d12fed3b
FATEMA JANNAT HIRA 242-15-351 <hira242-15-351@diu.edu.bd>
2026-08-13 22:43:13 +0600

    Updated Booking class

--- src/resortmanagemn/Booking.java
+++ src/resortmanagemn/Booking.java
@@ -6,15 +6,20 @@ package resortmanagemn;
 public class Booking {
     private Customer customer;
     private Room room;
+    private String checkInDate;
+    private String checkOutDate;
     private int days;
     
     private float totalCost;
 
-public Booking(Customer customer, Room room, int days) {
+public Booking(Customer customer, Room room, int days,String checkInDate,String checkOutDate)
+{
         this.customer = customer;
         this.room = room;
         this.days = days;
-  
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        
         this.totalCost = room.getPrice() * days;
         room.bookRoom();
     }
@@ -27,7 +32,17 @@ public Booking(Customer customer, Room room, int days) {
         return room;
     }
 
-   
+    public String getCheckInDate() {
+
+        return checkInDate;
+
+    }
+ public String getCheckOutDate() {
+
+        return checkOutDate;
+
+    }
+
     public int getDays() {
         return days;
     }
@@ -46,6 +61,9 @@ public Booking(Customer customer, Room room, int days) {
         return "Booking: " + customer +
                " | Room No: " + room.getRoomNumber() +
                " | Days: " + days +
+                 "\nCheck-in Date : " + checkInDate +
+
+        "\nCheck-out Date : " + checkOutDate +
                " | Total Cost: " + totalCost;
     }
 }
