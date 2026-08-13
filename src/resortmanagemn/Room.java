d111225986e37b991db5444d98907ca9792a64ab
Tanbin62 <251-15-418@diu.edu.bd>
2026-08-13 18:09:36 +0600

    Updated the toString in  Room Class

--- src/resortmanagemn/Room.java
+++ src/resortmanagemn/Room.java
@@ -1,14 +1,17 @@
 package resortmanagemn;
+
 enum RoomType {
     DELUXE,
     SUPER_DELUXE,
     KING_SIZE
 }
+
 enum RoomStatus {
     AVAILABLE,
     BOOKED,
     MAINTENANCE
 }
+
 public class Room {
 
     private int roomNumber;
@@ -34,53 +37,61 @@ public class Room {
 
         this.status = RoomStatus.AVAILABLE;
     }
-    public int getRoomNumber(){
+
+    public int getRoomNumber() {
         return roomNumber;
     }
-     public RoomType getType(){
+
+    public RoomType getType() {
         return type;
     }
-      public int getPrice(){
+
+    public int getPrice() {
         return price;
     }
-       public RoomStatus getRoomStatus(){
+
+    public RoomStatus getRoomStatus() {
         return status;
     }
-        public boolean isAvailable(){
-        return status == RoomStatus.AVAILABLE ;
+
+    public boolean isAvailable() {
+        return status == RoomStatus.AVAILABLE;
     }
-        
-        public boolean book(){
-            if(status == RoomStatus.AVAILABLE){
-                status = RoomStatus.BOOKED;
+
+    public boolean book() {
+        if (status == RoomStatus.AVAILABLE) {
+            status = RoomStatus.BOOKED;
             return true;
-            }
-            return false;
         }
-        
-        public boolean free(){
-            if(status == RoomStatus.BOOKED){
-                status = RoomStatus.AVAILABLE;
+        return false;
+    }
+
+    public boolean free() {
+        if (status == RoomStatus.BOOKED) {
+            status = RoomStatus.AVAILABLE;
             return true;
-            }
-            return false;
         }
-        
-        public boolean setMaintenance(){
-            if (status != RoomStatus.BOOKED) {
+        return false;
+    }
+
+    public boolean setMaintenance() {
+        if (status != RoomStatus.BOOKED) {
             status = RoomStatus.MAINTENANCE;
             return true;
         }
         return false;
-        }
-        
-        public boolean endMaintenance() {
+    }
+
+    public boolean endMaintenance() {
         if (status == RoomStatus.MAINTENANCE) {
             status = RoomStatus.AVAILABLE;
             return true;
         }
         return false;
     }
-}
 
-    
+    @Override
+    public String toString() {
+        return roomNumber + " : " + type + " : " + price + " : " + status;
+    }
+}
