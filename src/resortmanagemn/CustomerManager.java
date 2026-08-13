d50da25aab781c03576f957a3e8460b67a9b49d8
Marina Rahman Mitu <251-15-091@diu.edu.bd>
2026-08-13 17:48:54 +0600

    Added a isValidPhone() method and a validation check in addCustomer() method

--- src/resortmanagemn/CustomerManager.java
+++ src/resortmanagemn/CustomerManager.java
@@ -7,10 +7,26 @@ public class CustomerManager{
     private List<Customer>customerList = new ArrayList<>();
     
     public Customer addCustomer(String name,String nationalId,String phone){
+        if(!isValidPhone(phone)){
+            System.out.println("Invalid phoone no.Customer not created.");
+            return null;
+        }
         Customer newCustomer=new Customer(name,nationalId,phone);
         customerList.add(newCustomer);
         return newCustomer;
   
     
-
+    }
+    public boolean isValidPhone(String phone){
+        if(phone.length()!= 11){
+            return false;
+        }
+        for(int i=0;i<phone.length();i++){
+            if(!Character.isDigit(phone.charAt(i))){
+                return false;
+            }
+            return true;
+        }
+        
+    }
 }
