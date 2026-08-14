bd71f467522c894eabf5cf42a27123c5e1874b16
Ahanaf 584 <ahanaf242-15-584@diu.edu.bd>
2026-08-14 11:27:22 +0600

    Move PaymentSystem and ResortService into resortmanagemn package

--- src/resortmanagemn/Paymentsystem.java
+++ src/resortmanagemn/Paymentsystem.java
@@ -1,23 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package resortmanagemn;
 
-/**
- *
- * @author User
- */
-public class Paymentsystem {/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author User
- */
+public class Paymentsystem {
+
 
 
 public class Payment {
